8ab9db1a4938ac33dd7d3fa68d1e41cc84c5b81f
Blake Loring <blake_l@parsed.uk>
2015-06-25 10:06:36 +0100

    Create solver_basic.cpp

---
+++ src/solver_basic.cpp
@@ -0,0 +1,31 @@
+#include "solver.h"
+#include <stdio.h>
+#include <string.h>
+
+using namespace Simplex;
+
+int Solver::findBasicRow(Table& instance, int col) {
+	unsigned int count = 0;
+	int row;
+
+	//Assume row 0 is the results row
+	for (unsigned int i = 1; i < instance.getNumRows(); i++) {
+		if (instance.getField(i, col) != 0) {
+			count++;
+			row = i;
+		}
+	}
+	return count == 1 ? row : -1;
+}
+
+//TODO: Could be more efficient. The row->col->row search could be turned into just a row->col search
+int Solver::findBasic(Table& instance, int row) {
+
+	//-1 excludes the result row
+	for (unsigned int i = 1; i < instance.getNumColumns(); i++) {
+		if (findBasicRow(instance, i) != -1 && instance.getField(row, i) == 1) {
+			return i;
+		}
+	}
+	return -1;
+}
