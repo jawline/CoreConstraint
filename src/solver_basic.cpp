ca6b557c123022db797a1d902d0956f1ba72f1a9
Blake Loring <blake_l@parsed.uk>
2015-06-25 10:51:35 +0100

    Update solver_basic.cpp

--- src/solver_basic.cpp
+++ src/solver_basic.cpp
@@ -31,3 +31,48 @@ void Solver::findBasicInfo(Table& instance, int* rowBasicInfo) {
 		}
 	}
 }
+
+
+
+void Solver::tableBasicArtificialStep(Table& instance, int* rowBasis) {
+	
+	if (_excessiveLogging) {
+		printf("------------------------------------------\n");
+		printf("-             BASIC INFO                 -\n");
+		printf("------------------------------------------\n");
+	}
+	
+	//Calculate all existing basic data
+	findBasicInfo(instance, rowBasis);
+
+	//First row is the objective function, should have no basic variables
+	for (unsigned int i = 1; i < instance.getNumRows(); i++) {
+
+		//If a row has no basic column then insert an artificial variable to compensate
+		if (rowBasis[i] == -1) {
+			int col = instance.addColumn(std::string("artificial") + std::to_string(_lastArtificial++), true);
+			instance.setField(i, col, 1);
+			rowBasis[i] = col;
+			if (_excessiveLogging) {
+				printf("DEBUG: Failed to find basic variable for row %i\n", i);
+				printf("DEBUG: Creating artificial variable for row %i\n", i);
+				instance.print();
+			}
+		}
+		
+		if (_excessiveLogging) {
+			double basicField = instance.getField(i, rowBasis[i]);
+			double resultField = instance.getField(i, 0);
+			printf("DEBUG: Row %i: Col %i is basic (Solution: %f/%f -> %f)\n",
+				i,
+				rowBasis[i],
+				instance.getField(i, rowBasis[i]),
+				instance.getField(i, 0),
+				resultField == 0 ? 0 : basicField / resultField);
+		}
+	}
+	
+	if (_excessiveLogging) {
+		printf("------------------------------------------\n");
+	}
+}
