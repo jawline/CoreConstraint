784db4ca6c4dae9acdb8d3b5420880b296c7f443
Blake Loring <blake_l@parsed.uk>
2015-06-25 10:13:59 +0100

    Update solver_basic.cpp

--- src/solver_basic.cpp
+++ src/solver_basic.cpp
@@ -8,24 +8,24 @@ int Solver::findBasicRow(Table& instance, int col) {
 	unsigned int count = 0;
 	int row;
 
-	//Assume row 0 is the results row
 	for (unsigned int i = 1; i < instance.getNumRows(); i++) {
 		if (instance.getField(i, col) != 0) {
 			count++;
 			row = i;
 		}
 	}
+
 	return count == 1 ? row : -1;
 }
 
 //TODO: Could be more efficient. The row->col->row search could be turned into just a row->col search
 int Solver::findBasic(Table& instance, int row) {
 
-	//-1 excludes the result row
 	for (unsigned int i = 1; i < instance.getNumColumns(); i++) {
 		if (findBasicRow(instance, i) != -1 && instance.getField(row, i) == 1) {
 			return i;
 		}
 	}
+
 	return -1;
 }
